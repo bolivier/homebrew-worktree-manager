d37d5a9e0e6599d816193df174b5ad0dfb5f6dd7
bolivier <2995224+bolivier@users.noreply.github.com>
2025-04-30 17:44:10 +0000

    [create-pull-request] automated change

--- Formula/worktree-manager.rb
+++ Formula/worktree-manager.rb
@@ -1,8 +1,8 @@
 class WorktreeManager < Formula
   desc "A CLI tool for managing Git worktrees"
   homepage "https://github.com/bolivier/worktree-manager"
-  url "https://github.com/bolivier/worktree-manager/releases/download/v1.0.0/worktree-manager.tar.gz"
-  sha256 "860d914afa4b264952df3425aadd0f8971d7dd683f4be0fe4574184443d5664d"
+  url "https://github.com/bolivier/worktree-manager/releases/download/v1.0.1/worktree-manager.tar.gz"
+  sha256 "82aed39c5fc8f73f4b6d9ac03e7b1a767ad7248f2da54538bd88f05b6954cdcf"
   license "MIT"
 
   depends_on "babashka"
