6c34158f2a3746d8c79743617b36cfc8aa8df568
Brandon Olivier <brandon@brandonolivier.com>
2025-04-30 10:26:49 -0500

    Add placeholders for computation of dynamic values
    I'm adding this because I want to setup CI in the main worktree-manager
    repo.

--- Formula/worktree-manager.rb
+++ Formula/worktree-manager.rb
@@ -1,10 +1,8 @@
-#!/usr/bin/env ruby
-
 class WorktreeManager < Formula
   desc "A CLI tool for managing Git worktrees"
   homepage "https://github.com/bolivier/worktree-manager"
-  url "https://github.com/bolivier/worktree-manager/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "17269cf68804c0435bf256e598e5c621327e5979109e274cbfb34c4d275503ac"
+  url "https://github.com/bolivier/worktree-manager/releases/download/__VERSION__/worktree-manager.tar.gz"
+  sha256 "__SHA__"
   license "MIT"
 
   depends_on "babashka"
